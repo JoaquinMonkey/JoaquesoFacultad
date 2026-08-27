21b5f73de0ae917a6c3b6bed4f8e01b93827f332
Joaquin <joaquinfontes4@gmail.com>
2026-08-27 14:28:37 -0300

    prueba

--- src/logica/ManejadorDiasConsulta.java
+++ src/logica/ManejadorDiasConsulta.java
@@ -1,5 +1,40 @@
 package logica;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class ManejadorDiasConsulta {
 
+    private Map<String, Dia_consulta> diasConsulta;
+    private static ManejadorDiasConsulta instancia = null;
+
+    private ManejadorDiasConsulta() {
+        diasConsulta = new HashMap<String, Dia_consulta>();
+    }
+
+    public static ManejadorDiasConsulta getinstance() {
+        if (instancia == null)
+            instancia = new ManejadorDiasConsulta();
+        return instancia;
+    }
+
+    public void addDiaConsulta(Dia_consulta dc) {
+        diasConsulta.put(dc.getNombre(), dc);
+    }
+
+    public boolean existeDiaConsulta(String nombre) {
+        return diasConsulta.get(nombre) != null;
+    }
+
+    public Dia_consulta getDiaConsulta(String nombre) {
+        return diasConsulta.get(nombre);
+    }
+
+    public Set<Dia_consulta> getDiasConsulta() {
+        Collection<Dia_consulta> dias = diasConsulta.values();
+        return new HashSet<Dia_consulta>(dias);
+    }
 }
